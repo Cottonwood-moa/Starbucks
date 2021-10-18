498bd4a474f0b003aeaddaf4824dc5392dc6693f
Cottonwood <blog_geon@naver.com>
2021-10-18 12:48:59 +0900

    no message

---
+++ js/main.js
@@ -0,0 +1,157 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션(객체));
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    // 버튼(to-top) 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0,
+    });
+  } else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+    // 버튼(to-top) 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100,
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간) *loadash.js cdn 필요 -> 스크롤시 함수가 많이 실행되는것을 방지
+// opacity 속성처럼 값을 숫자로 입력하는 속성들은, 전환효과(transition 속성이나 GSAP 라이브러리 등)를 통해
+// 요소의 전/후 상태를 중간 숫자의 값으로 자연스럽게 만들어 줄 수 있지만,
+// display 속성처럼 값이 숫자가 아닌 속성은 전/후 상태의 중간값이 존재하지 않기 때문에, 자연스러운 전환 효과를 적용할 수 없다.
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .5, {
+    scrollTo: 0 //scrollToPlugin cdn 필요
+  })
+})
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션(객체));
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+new Swiper('.notice-line .swiper-container', {
+  // Optional parameters
+  direction: 'vertical',
+  autoplay: true, //자동 재생 여부
+  loop: true //반복 여부
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 시야 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데로 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000 //5초, 디폴트 3초
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+new Swiper('.awards .swiper-container', {
+  direction: 'horizontal',
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+
+let isHidePromotion = false; //프로모션이 안숨겨져있다.
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion; //false이면 true, ture이면 false
+  if (isHidePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 지속시간, 옵션(객체));
+  gsap.to(
+    selector, //선택자
+    random(1.5, 2.5), //애니메이션 동작 시간
+    { //옵션
+      y: size,
+      repeat: -1, //무한반복
+      yoyo: true,
+      ease: Power1.easeInOut, //gsap easing
+      delay: random(0, delay)
+    });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+
+thisYear.textContent = new Date().getFullYear(); // 2021
